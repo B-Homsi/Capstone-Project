447a05fe377810fe14c2bbbac48abc9a453dd62b
B-Homsi <bhomsi96@gmail.com>
2023-03-21 15:40:48 +0100

    removeUseCallback

--- hooks/useSubjectUpdater.js
+++ hooks/useSubjectUpdater.js
@@ -1,60 +1,53 @@
-import { useCallback } from "react";
 import { uid } from "uid";
 
 export const useSubjectUpdater = (subjects, setSubjects) => {
-  const addCard = useCallback(
-    (card, subjectId) => {
-      const newCard = { ...card, id: uid() };
+  const addCard = (card, subjectId) => {
+    const newCard = { ...card, id: uid() };
 
-      const updatedSubjects = subjects.map((s) => {
-        if (s.id === subjectId) {
-          const updatedTopics = s.topics.map((topic) => {
-            if (topic.title === card.selectedTopic) {
-              return {
-                ...topic,
-                cards: [...(topic.cards || []), newCard],
-              };
-            }
-            return topic;
-          });
-
-          return {
-            ...s,
-            topics: updatedTopics,
-          };
-        }
-        return s;
-      });
-
-      setSubjects(updatedSubjects);
-    },
-    [subjects, setSubjects]
-  );
-
-  const deleteCard = useCallback(
-    (cardId) => {
-      const updatedSubjects = subjects.map((s) => {
+    const updatedSubjects = subjects.map((s) => {
+      if (s.id === subjectId) {
         const updatedTopics = s.topics.map((topic) => {
-          const shouldRemoveCard = topic.cards?.some(
-            (card) => card.id === cardId
-          );
-
-          if (shouldRemoveCard) {
+          if (topic.title === card.selectedTopic) {
             return {
               ...topic,
-              cards: topic.cards.filter((card) => card.id !== cardId),
+              cards: [...(topic.cards || []), newCard],
             };
           }
           return topic;
         });
 
-        return { ...s, topics: updatedTopics };
+        return {
+          ...s,
+          topics: updatedTopics,
+        };
+      }
+      return s;
+    });
+
+    setSubjects(updatedSubjects);
+  };
+
+  const deleteCard = (cardId) => {
+    const updatedSubjects = subjects.map((s) => {
+      const updatedTopics = s.topics.map((topic) => {
+        const shouldRemoveCard = topic.cards?.some(
+          (card) => card.id === cardId
+        );
+
+        if (shouldRemoveCard) {
+          return {
+            ...topic,
+            cards: topic.cards.filter((card) => card.id !== cardId),
+          };
+        }
+        return topic;
       });
 
-      setSubjects(updatedSubjects);
-    },
-    [subjects, setSubjects]
-  );
+      return { ...s, topics: updatedTopics };
+    });
+
+    setSubjects(updatedSubjects);
+  };
 
   return { addCard, deleteCard };
 };
