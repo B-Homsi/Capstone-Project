b819ccdb710192b1b302d8d927e42e4be3f4b863
B-Homsi <bhomsi96@gmail.com>
2023-04-03 13:28:08 +0200

    Statistics

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,10 +1,36 @@
 import Header from "@/components/Header/Header";
+import styled from "styled-components";
 
-export default function Profile() {
+export default function Profile({ subjects }) {
+  const totalCards = subjects.reduce((total, subject) => {
+    return (
+      total +
+      subject.topics.reduce((topicTotal, topic) => {
+        return topicTotal + (topic.cards ? topic.cards.length : 0);
+      }, 0)
+    );
+  }, 0);
+
   return (
     <>
       <Header>Profile</Header>
-      <main></main>
+      <Main>
+        <H2>Statistics: </H2>
+        <P>Total flashcards: {totalCards}</P>
+      </Main>
     </>
   );
 }
+
+const Main = styled.main`
+  color: #fff;
+  margin: 0 20px;
+`;
+
+const H2 = styled.h2`
+  color: #fff;
+`;
+
+const P = styled.p`
+  color: #fff;
+`;
